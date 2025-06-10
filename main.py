68994440b5c1049337f92c1993c7f591323d2f7a
luis.vespa <luis.vespa@digidentity.com>
2025-06-10 02:13:32 +0200

    deploy bot

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def run_bot_scheduler():
 
 if __name__ == "__main__":
     # Lanza el panel admin Flask
-    run_admin_panel()
+    #run_admin_panel()
     #run_bot_scheduler()
     # Lanza el bot como hilo en segundo plano
-    #Thread(target=run_bot_scheduler, daemon=True).start()
+    Thread(target=run_bot_scheduler, daemon=True).start()
